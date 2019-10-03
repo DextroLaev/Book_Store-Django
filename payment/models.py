64ef3cfa6679911687bb3b05028ca7dcb7e8c2a6
Rajshekhar126 <rajshekharrakshit123@gmail.com>
2019-10-03 11:06:00 +0530

    update in payment model

--- payment/models.py
+++ payment/models.py
@@ -8,6 +8,12 @@ class Payment(models.Model):
     state = models.CharField(max_length=60)
     zipcode = models.IntegerField(default=0)
     phone = models.IntegerField(default=0)
+    card_holder_name = models.CharField(max_length=60)
+    card_no = models.CharField(max_length=21)
+    product_id= models.IntegerField(default=0)
+    
+    card_day = models.IntegerField(default=0)
+    card_month = models.IntegerField(default=0)
     payment_type = models.CharField(max_length=20)
 
     def __str__(self):
--- payment/views.py
+++ payment/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect
 from . models import Payment
-
+from books.models import Books
 
 # Create your views here.
-def payment_gateway(request):
-    return render(request,'payment/payment_gateway.html')
+def payment_gateway(request,product_id):
+    product = Books.objects.get(id=product_id)
+    return render(request,'payment/payment_gateway.html',{'product':product})
 
-def buy(request):
+def buy(request,product_id):
     if request.method=='POST':
         user = Payment()
         user.name = request.POST['name']
@@ -15,8 +16,22 @@ def buy(request):
         user.state = request.POST['state']
         user.zipcode = request.POST['zipcode']
         user.phone = request.POST['phone']
+        user.card_holder_name = request.POST['cardname']
+        user.card_day = request.POST['carddate']
+        user.card_month = request.POST['cardmonth']
+        user.product_id = product_id
         if request.POST.get('debitcard','on'):
             user.payment_type = "Debit card/Credit card"
+            card_number = request.POST['cardno']
+            for i in range(len(card_number)):
+                if i==0:
+                    user.card_no += card_number[i]
+                elif i%4==0:
+                    user.card_no += ' '
+                    user.card_no += card_number[i]
+                else:
+                    user.card_no += card_number[i]        
+                    
         else:
             user.payment_type = 'COD'
 
