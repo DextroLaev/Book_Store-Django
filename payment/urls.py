9c393f6004e605f2eb115f1b224959a2e19a629a
Rajshekhar Rakshit <44888331+DextroLaev@users.noreply.github.com>
2021-05-03 14:45:22 +0530

    update2

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('payment_gateway/<int:product_id>/',views.payment_gateway,name='payment_gateway'),
     path('buy/<int:product_id>/<int:user_id>/',views.buy,name='buy'),
-    path('add_cart/<int:product_id>/',views.add_cart,name='add_cart')
+    path('add_cart/<int:product_id>/<int:user_id>/',views.add_cart,name='add_cart')
    
 ]
