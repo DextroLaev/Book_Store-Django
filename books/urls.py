89fbe18a6df3e4370a78d10e05176db47b233ef1
Rajshekhar Rakshit <44888331+DextroLaev@users.noreply.github.com>
2021-05-03 14:43:54 +0530

    update2

--- books/urls.py
+++ books/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('orders/<int:user_id>',views.orders,name='orders'),
     path('about/',views.about,name='about'),
-    path('cart/',views.cart,name='cart'),
+    path('cart/<int:user_id>',views.cart,name='cart'),
     path('service/',views.service,name='service'),
     path('addproducts/',views.addproducts,name='addproducts'),
-    path('delete_product/<int:delete_id>',views.delete,name='delete_product')
+    path('delete_product/<int:delete_id>',views.delete,name='delete_product'),
+    path('remove_product/<int:remove_id>/<int:user_id>',views.remove_cart,name='remove_product')
    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
